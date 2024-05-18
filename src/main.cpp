5dedf66c9b396b0b123ebefdb124fe6a6cfb0206
AdamP <adampinnock10@gmail.com>
2024-05-18 22:29:46 +0100

    Swapped uncommented case to NORM

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,10 @@
 int main() {
     GameEngine engine;
     // NORM //
-    //engine.run();
+    engine.run();
 
     // TEST //
-    engine.runSimulation(10000);
+    //engine.runSimulation(10000);
     //e.g. run the game for 10000 updates/steps
 
     return 0;
