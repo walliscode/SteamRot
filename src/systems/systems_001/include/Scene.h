5dea44d379cf2d840dfc997647838cb826d19a69
walliscode <148866497+walliscode@users.noreply.github.com>
2024-05-25 16:06:16 +0100

    Update src/systems/systems_001/include/Scene.h
    Co-authored-by: NHughes99 <125889316+NHughes99@users.noreply.github.com>

--- src/systems/systems_001/include/Scene.h
+++ src/systems/systems_001/include/Scene.h
@@ -4,7 +4,7 @@ class Scene {
 protected:
 
 	bool    m_paused = false; // If the scene is paused
-	bool    m_active = true; // if the scene is active, should the Scene manager call the updated funciton
+	bool    m_active = true; // if the scene is active, should the Scene manager call the updated function
 	size_t  m_current_frame = 0; // The current frame of the scene, used for animations and systems logic
 
 public:
