bbe32c026bc28f576510fa1fb7af9f8bdea13c51
Technuts101 <141076517+Technuts101@users.noreply.github.com>
2024-06-04 23:02:31 +0000

    Initial

---
+++ src/Core/Archetype.h
@@ -0,0 +1,27 @@
+#ifndef ARCHETYPE_H
+#define ARCHETYPE_H
+
+#include <vector>
+
+class Archetype {
+private:
+    std::vector<int> entityIDs;
+        unsigned int id;
+
+        public:
+            Archetype(unsigned int archetypeId) : id(archetypeId) {}
+
+                unsigned int getId() const {
+                        return id;
+                            }
+
+                                const std::vector<int>& getEntityIDs() const {
+                                        return entityIDs;
+                                            }
+
+                                                void setEntityIDs(const std::vector<int>& newEntityIDs) {
+                                                        entityIDs = newEntityIDs;
+                                                            }
+                                                            };
+
+                                                            #endif // ARCHETYPE_H
