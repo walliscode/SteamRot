0b0d08c9d87056e33a9202807ac5398a5d69f065
walliscode <148866497+walliscode@users.noreply.github.com>
2024-06-05 20:55:56 +0100

    Update src/components/components001/include/CMeta.h
    Co-authored-by: NHughes99 <125889316+NHughes99@users.noreply.github.com>

--- src/components/components001/include/CMeta.h
+++ src/components/components001/include/CMeta.h
@@ -11,7 +11,7 @@ public:
 
 	const bool getActive() const; // used to determine if the entity is active and will be considered in systems
 
-	void setAlive(); // used to set the entity as active
+	void activate(); // used to set the entity as active
 	void deactivate(); // used to set the entity as inactive
 
 	CMeta() = default; // default constructor for memory allocation
