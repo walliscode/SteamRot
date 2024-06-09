b10c82f7b58c8d7fc109c0dffd7bcaf1d86951a2
Ben Wallis <benjaminwallisuk@gmail.com>
2024-06-09 16:56:10 +0100

    set up auto generation of flatbuffers

--- cmake/FetchContentModules/Addflatbuffers.cmake
+++ cmake/FetchContentModules/Addflatbuffers.cmake
@@ -23,17 +23,53 @@ find_package(FlatBuffers) # needs to be FlatBuffers, not flatbuffers as FindFlat
 
 message(STATUS "FLATBUFFERS_FOUND: ${FLATBUFFERS_FOUND}")
 
-if(FLATBUFFERS_FOUND)
-	# set variables
+########## Generating Data Files ##########
+# This next bit loops through a curated list (DATA_FILES) of data files that we want to generate binary and header files)
 
 
-	if(COMMAND build_flatbuffers)
-	  message(STATUS "##### build_flatbuffers found, converting .fbs to headers and .json to binaries #####")
+if(FLATBUFFERS_FOUND) # only runs if flatbuffers has been built
 
-	else()
-	  message(STATUS "The build_flatbuffers function is not defined.")
-	endif()
+message("###### Generating Data Files ######")
+
+######### Curated List of Data Files #########
+set(DATA_FILES 
+    "basic_data"
+)
+#############################################
+
+foreach(NAME ${DATA_FILES})
+set(FBS_FILE ${CMAKE_CURRENT_SOURCE_DIR}/resources/schemas/${NAME}.fbs)
+set(JSON_FILE ${CMAKE_CURRENT_SOURCE_DIR}/resources/jsons/${NAME}.json)
+set(BINARY_FILE ${CMAKE_CURRENT_SOURCE_DIR}/resources/binaries/${NAME}.bin)
+set(HEADER_FILE ${CMAKE_CURRENT_SOURCE_DIR}/resources/generated_headers/${NAME}_generated.h)
+
+if(EXISTS ${FBS_FILE} AND EXISTS ${JSON_FILE})
+    # output success message if they exist
+    message(STATUS "Found FlatBuffers schema and JSON file for ${NAME}")
+    # Generate the binary file
+    add_custom_command(
+    OUTPUT ${BINARY_FILE}
+    COMMAND ${FLATBUFFERS_FLATC_EXECUTABLE} -o ${CMAKE_CURRENT_SOURCE_DIR}/resources/binaries -b ${FBS_FILE} ${JSON_FILE}
+    DEPENDS ${JSON_FILE}
+    COMMENT "Generating binary FlatBuffers file from ${JSON_FILE}"
+    )
+    list(APPEND GENERATED_FILES ${BINARY_FILE})
+
+    # Generate the header file
+    add_custom_command(
+    OUTPUT ${HEADER_FILE}
+    COMMAND ${FLATBUFFERS_FLATC_EXECUTABLE} -o ${CMAKE_CURRENT_SOURCE_DIR}/resources/generated_headers -c ${FBS_FILE}
+    DEPENDS ${FBS_FILE}
+    COMMENT "Generating FlatBuffers header file from ${FBS_FILE}"
+    )
+
+    list(APPEND GENERATED_FILES ${HEADER_FILE})
+    
+endif()
+endforeach()
+
+# by generating a custom target, we can ensure that the generated files are built. (https://cmake.org/cmake/help/book/mastering-cmake/chapter/Custom%20Commands.html#:~:text=add_custom_command%20has%20two%20main%20signatures,to%20add%20the%20custom%20command.)
+add_custom_target(my_binary_data ALL 
+    DEPENDS ${GENERATED_FILES})
 
 endif()
-
-
