0d09041b212fb04edb4da41ab45f6c72ada5b449
Ben Wallis <benjaminwallisuk@gmail.com>
2024-06-16 09:17:31 +0100

    added route for loading fonts

--- src/components/assets/Assets.cpp
+++ src/components/assets/Assets.cpp
@@ -1,5 +1,8 @@
 #include "Assets.h" // for some reason, the CMake file won't link assets.h without the include
 #include <iostream>
+#include <fstream>
+
+#include "load_fonts_generated.h"
 
 
 Assets::Assets() {
@@ -11,4 +14,50 @@ void Assets::init() {
 
 	std::cout << "####### Loading assets... ########" << std::endl;
 	// Load assets here
+	loadFonts((std::string(RESOURCES_PATH) +   "binaries/load_fonts.bin")); // pass the binary that contains the json configurations
+}
+
+void Assets::loadFonts(const std::string& filename)
+{
+	if (!fileExists(filename)) {
+		std::cout << "File does not exist: " << filename << std::endl;
+		return;
+	}
+	std::cout << "#########  Loading fonts from  #########" << filename << std::endl;
+
+	std::ifstream infile;
+	infile.open(filename, std::ios::binary | std::ios::in);
+	infile.seekg(0, std::ios::end);
+	int length = infile.tellg();
+	infile.seekg(0, std::ios::beg);
+	char* data = new char[length];
+	infile.read(data, length);
+	infile.close();
+
+	const SteamRot::rawData::FontList* font_list = SteamRot::rawData::GetFontList(data);
+
+	for (const auto font : *font_list->fonts()) {
+		addFont(font->tag()->str(), font->fileName()->str());
+	}
+}
+
+void Assets::addFont(const std::string& tag, const std::string& fileName)
+{
+	sf::Font font;
+	std::string path = std::string(RESOURCES_PATH) + "fonts/" +  fileName;
+	font.loadFromFile(path);
+	m_fonts[tag] = font;
+	std::cout << "Loaded font: " << tag << std::endl;
+}
+
+const sf::Font& Assets::getFont(const std::string& name) const {
+
+	// Get a font
+	return m_fonts.at(name);
+}
+
+bool Assets::fileExists(const std::string& filename) const
+{
+	std::ifstream file(filename);
+	return file.good();
 }
--- src/components/assets/include/Assets.h
+++ src/components/assets/include/Assets.h
@@ -1,11 +1,24 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+
 
 class Assets {
 
 private:
+	std::map<std::string, sf::Font>              m_fonts;
+
 	void init();
 
 public:
 	Assets();
+
+	//###### Fonts ######
+	void loadFonts(const std::string& filename); // load assets from a FlatBuffers file
+	void addFont(const std::string& tag, const std::string& fileName); // add a font
+	const sf::Font& getFont(const std::string& name) const; // get a font
+
+
+	//###### Helper functions ######
+	bool fileExists(const std::string& filename) const; // check if a file exists
 	
 };
