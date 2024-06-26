fe1e09d9dadc1b4e38587721d4864ab808251dc6
= <n.j.hughes956@gmail.com>
2024-06-26 19:39:46 +0100

    Initial changes to data types and functions to add and remove entities from the lists

--- src/entity/Archetype.cpp
+++ src/entity/Archetype.cpp
@@ -1,12 +1,20 @@
 #pragma once
 #include "Archetype.h"
 
-Archetype::Archetype(unsigned int archetypeID) : m_id(archetypeID) {}
+Archetype::Archetype(std::vector<size_t> idSet) : m_id(idSet) {}
 
-const unsigned int Archetype::getID() const {
-	return m_id;
+const std::vector<size_t>& Archetype::getIDSet() const {
+	return m_idSet;
 }
 
-const std::vector<int>& Archetype::getEntityIDs() const {
-	return m_entityIDs;
+const std::vector<size_t>& Archetype::getEntities() const {
+	return m_entities;
+}
+
+void Archetype::addEntity(size_t newEntity) {
+	m_entities.push_back(newEntity);
+}
+
+void Archetype::removeEntity(size_t remEntity) {
+	m_entities.erase(std::remove(m_entities.begin(), m_entities.end(), remEntity, m_entities.end());
 }
