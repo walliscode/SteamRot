d0f671b3f4bb620adca6957356bd6c07107a0dc4
= <n.j.hughes956@gmail.com>
2024-06-28 20:14:10 +0100

    Added a getcode function to the archetype which decodes the ID's and returns the binary string. Added the tests to add and remove entity Id's and confirm the archetype code

--- src/entity/Archetype.cpp
+++ src/entity/Archetype.cpp
@@ -1,7 +1,7 @@
 #pragma once
 #include "Archetype.h"
 
-Archetype::Archetype(std::vector<size_t> idSet) : m_id(idSet) {}
+Archetype::Archetype(std::vector<size_t> idSet) : m_idSet(idSet) {}
 
 const std::vector<size_t>& Archetype::getIDSet() const {
 	return m_idSet;
@@ -16,5 +16,13 @@ void Archetype::addEntity(size_t newEntity) {
 }
 
 void Archetype::removeEntity(size_t remEntity) {
-	m_entities.erase(std::remove(m_entities.begin(), m_entities.end(), remEntity, m_entities.end());
+	m_entities.erase(std::remove(m_entities.begin(), m_entities.end(), remEntity), m_entities.end());
+}
+
+const size_t& Archetype::getCode() const {
+	size_t archCode = 0;
+	for (auto& compID : m_idSet) {
+		archCode = archCode | (1 << (compID - 1)); //for each ID, OR the current code with 1, bitshifted by the ID - 1 (add a 1 to the code at the binary position set by the ID)
+	};
+	return archCode;
 }
--- src/entity/include/Archetype.h
+++ src/entity/include/Archetype.h
@@ -13,6 +13,7 @@ public:
     const std::vector<size_t>& getIDSet() const; //get the archetype key set
     const std::vector<size_t>& getEntities() const; //get the set of entity ids in this archetype
     void addEntity(size_t newEntity); //add an entity ID to this archetype
-    void removeEntity(size_t remEntity) //remove an entity ID from this archetype
+    void removeEntity(size_t remEntity); //remove an entity ID from this archetype
 
+    const size_t& getCode() const;
 };
