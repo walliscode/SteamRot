4976bb938f18ce57025b4552e4ecb428f5ff2a57
Ben Wallis <benjaminwallisuk@gmail.com>
2025-02-05 20:46:49 +0000

    added event handling for keyboard and mouse

--- include/constants/global_constants.h
+++ include/constants/global_constants.h
@@ -4,6 +4,6 @@ namespace SteamRot {
 
 // the total number of inputs the user can use from the keyboard and mouse.
 size_t const kUserInputCount{sf::Keyboard::ScancodeCount +
-                             sf::Mouse::ButtonCount + 1};
+                             sf::Mouse::ButtonCount};
 
 } // namespace SteamRot
