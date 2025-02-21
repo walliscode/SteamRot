58b5f2f72622bcd62f3002cf416d2df61f7a6eff
Ben Wallis <benjaminwallisuk@gmail.com>
2025-02-21 22:15:57 +0000

    added initial testing structure

---
+++ tests/unit_tests/GameEngine.test.cpp
@@ -0,0 +1,35 @@
+#include "GameEngine.h"
+#include <catch2/catch_test_macros.hpp>
+#include <iostream>
+
+TEST_CASE("Simulation is run", "[GameEngine]") {
+
+  std::cout << "********************** Simulation Tests *********************"
+            << std::endl;
+  GameEngine game;
+  int num = 0;
+  REQUIRE(num == 1);
+  std::cout << "Running the simulation for 0 loops\n";
+  REQUIRE_THROWS(game.runSimulation(
+      0)); // Check that the simulation does not run if loops = 0
+  REQUIRE(game.getLoopNumber() == 0);
+  std::cout << "Running the simulation for -5 loops\n";
+  REQUIRE_THROWS(game.runSimulation(
+      -5)); // Check that the simulation does not run if loops < 0
+  REQUIRE(game.getLoopNumber() == 0);
+
+  std::cout << "Running the simulation for 10 loops\n";
+  game.runSimulation(10);
+  REQUIRE(game.getLoopNumber() ==
+          10); // Check that the simulation runs for 10 loops
+  //
+  // std::cout << "Creating a JSON file and checking contents\n";
+  // game.runSimulation(24);
+  // json sim_json = game.extractJSON("simulations", "test_data");
+  // REQUIRE(sim_json["GameEngine"]["loopNumber"] ==
+  //         24); // Check that the loop number is 24
+  //
+  std::cout
+      << "********************** Simulation Tests END *********************"
+      << std::endl;
+};
