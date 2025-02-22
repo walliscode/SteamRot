e99f9e3ec6d6a4d22bdba3bd15fb767a840e30cb
Ben Wallis <benjaminwallisuk@gmail.com>
2025-02-22 18:33:05 +0000

    added json structure testin

---
+++ tests/unit_tests/test_json_structure.test.cpp
@@ -0,0 +1,30 @@
+#include "GameEngine.h"
+#include "TestUtility.h"
+#include <catch2/catch_test_macros.hpp>
+#include <filesystem>
+#include <fstream>
+
+namespace fs = std::filesystem;
+
+// the GameEngine ExtractJSON will output the whole game state to a json file
+// this test is to check that the outputted json file has the correct structure
+// NOT to check the values of the data
+//
+// Each time a new data type is extracted, this test should be updated
+TEST_CASE("test json structure", "[data_out]") {
+
+  // set up resources to test (this should just be the whole game)
+  GameEngine game;
+  game.runSimulation(10);
+
+  // define json structure to test against
+  json defined_structure = {{"GameEngine", {{"m_loop_number", nullptr}}}};
+
+  // import the test json file
+  fs::path json_file_path = fs::path(DATA_OUT_DIR) / "test.json";
+  std::ifstream f(json_file_path);
+  json test_structure = json::parse(f);
+
+  // check the structure using a recurvisve function
+  TestUtility::check_structure(defined_structure, test_structure);
+};
