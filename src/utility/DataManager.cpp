c336ae83c32a4e583b4833cbefea0cb7af4980df
Ben Wallis <benjaminwallisuk@gmail.com>
2025-05-04 17:33:20 +0100

    added CheckFileExists and LoadJSONData function

--- src/utility/DataManager.cpp
+++ src/utility/DataManager.cpp
@@ -1,6 +1,8 @@
 #include "DataManager.h"
 #include "SchemaChecker.h"
-#include <expected>
+#include "log_handler.h"
+#include <fstream>
+
 ////////////////////////////////////////////////////////////
 void DataManager::LoadData() {};
 
@@ -12,4 +14,31 @@ EntityConfigData DataManager::LoadSceneDataFromJson(nlohmann::json scene_data) {
   bool proceed = schema_checker.CheckJSON(scene_data);
 
   return EntityConfigData(scene_data);
+}
+
+////////////////////////////////////////////////////////////
+void DataManager::CheckFileExists(const std::string &file_path) {
+
+  if (!std::filesystem::exists(file_path)) {
+    // generate error message
+    std::string message{"Target file does not exist: " + file_path};
+
+    // pass message through the log handler
+    steamrot::log_handler::ProcessLog(
+        spdlog::level::err, steamrot::log_handler::LogCode::kFileNotFound,
+        message);
+  }
+};
+
+////////////////////////////////////////////////////////////
+nlohmann::json DataManager::LoadJsonData(const std::string &file_path) {
+
+  // check file exists, this is a go/no go checkpoint
+  CheckFileExists(file_path);
+
+  // load json data from file
+  std::ifstream file(file_path);
+
+  // return json object
+  return nlohmann::json::parse(file);
 }
