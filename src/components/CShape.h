6f9ed7cef65e05731fbe3ee6836927fac925e02d
Ben Wallis <benjaminwallisuk@gmail.com>
2025-05-17 11:12:47 +0100

    update CMeta Configure function

--- src/components/CShape.h
+++ src/components/CShape.h
@@ -6,8 +6,6 @@ class CShape : public Component {
 public:
   CShape() = default;
 
-  sf::RectangleShape m_shape;
-
   const std::string &Name() override;
   void Configure(const json &data) override;
 };
