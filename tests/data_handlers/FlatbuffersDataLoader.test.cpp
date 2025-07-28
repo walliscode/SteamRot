572168df3d81547de3484eba847d821734363b23
walliscode <benjaminwallisuk@gmail.com>
2025-07-28 10:01:57 +0100

    DataLoader initiates PathProvider only once

--- tests/data_handlers/FlatbuffersDataLoader.test.cpp
+++ tests/data_handlers/FlatbuffersDataLoader.test.cpp
@@ -9,12 +9,25 @@
 #include "FlatbuffersDataLoader.h"
 #include <catch2/catch_test_macros.hpp>
 
-// create FlatbuffersDataLoader object
-steamrot::FlatbuffersDataLoader flatbuffersDataLoader;
+TEST_CASE("Dataloader fails to initiate PathProvider", "[DataLoader]") {
+  REQUIRE_THROWS(
+      steamrot::FlatbuffersDataLoader{steamrot::EnvironmentType::None});
+}
 
-TEST_CASE("fails to find binary file for fragment data",
-          "[FlatbuffersDataLoader]") {
+TEST_CASE("DataLoader can be initiated with a valid environment",
+          "[DataLoader]") {
+  REQUIRE_NOTHROW(
+      steamrot::FlatbuffersDataLoader{steamrot::EnvironmentType::Test});
+}
 
-  auto result = flatbuffersDataLoader.ProvideFragment("non_existent_fragment");
-  REQUIRE(result.error() == steamrot::DataFailMode::FileNotFound);
+TEST_CASE("DataLoader can be called with a None environment after being set",
+          "[DataLoader]") {
+  steamrot::FlatbuffersDataLoader data_loader{steamrot::EnvironmentType::Test};
+  REQUIRE_NOTHROW(steamrot::FlatbuffersDataLoader{});
+}
+
+TEST_CASE("DataLoader can only initiate PathProvider once", "[DataLoader]") {
+  steamrot::FlatbuffersDataLoader data_loader{steamrot::EnvironmentType::Test};
+  REQUIRE_THROWS(
+      steamrot::FlatbuffersDataLoader{steamrot::EnvironmentType::Production});
 }
