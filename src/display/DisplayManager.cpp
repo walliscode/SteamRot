1bee416cffdfc22b2c2ee25ebf95676d4bd8779a
walliscode <benjaminwallisuk@gmail.com>
2025-08-24 13:03:15 +0100

    CallRenderCycle added with no errors

--- src/display/DisplayManager.cpp
+++ src/display/DisplayManager.cpp
@@ -9,6 +9,7 @@
 #include "DisplayManager.h"
 #include "Session.h"
 #include <SFML/Graphics/RenderTexture.hpp>
+#include <variant>
 
 namespace steamrot {
 
@@ -18,4 +19,18 @@ DisplayManager::DisplayManager(sf::RenderWindow &window,
     : m_active_session(std::make_shared<Session>()), m_window(window),
       m_scene_manager_interface(scene_manager_interface) {};
 
+/////////////////////////////////////////////////
+std::expected<std::monostate, FailInfo> DisplayManager::CallRenderCycle() {
+  // clear the window
+  m_window.clear(sf::Color::Black);
+
+  // ADD CODE BETWEEN HERE
+  // add container/wrapper functions for drawing here
+
+  // AND HERE
+  // display the window
+  m_window.display();
+
+  return std::monostate{};
+}
 } // namespace steamrot
