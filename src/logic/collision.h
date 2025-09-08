17de9be58cc070e637e031de6a6f025808d17984
walliscode <benjaminwallisuk@gmail.com>
2025-09-08 20:53:13 +0100

    nested check of mouse collision added

--- src/logic/collision.h
+++ src/logic/collision.h
@@ -23,8 +23,27 @@ namespace collision {
 bool IsMouseOverBounds(const sf::Vector2i &mouse_position,
                        const sf::FloatRect &bounds);
 
+/////////////////////////////////////////////////
+/// @brief Checks if the mouse is over a given UI element and updates its state
+/// accordingly.
+///
+/// @param mouse_position The current global mouse position
+/// @param element UiElement to check against
+/////////////////////////////////////////////////
 void CheckMouseOverUIElement(const sf::Vector2i &mouse_position,
                              UIElement &element);
+
+/////////////////////////////////////////////////
+/// @brief Checks if the mouse is over a given UI element and its nested
+/// children,
+///
+/// This functions checks an element and all children, if a child is hovered, it
+/// updates the parent to false.
+/// @param mouse_position The current global mouse position
+/// @param element UIElement to check against
+/////////////////////////////////////////////////
+void CheckMouseOverNestedUIElement(const sf::Vector2i &mouse_position,
+                                   UIElement &element);
 
 } // namespace collision
 } // namespace steamrot
