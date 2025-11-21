0af26da7bd43f424fda18db5eb74b3d7719014af
walliscode <benjaminwallisuk@gmail.com>
2025-11-21 18:18:37 +0000

    old files removed and tests got working

--- tests/unit/components/CUserInterface.test.cpp
+++ tests/unit/components/CUserInterface.test.cpp
@@ -7,21 +7,12 @@
 /// Headers
 /////////////////////////////////////////////////
 #include "CUserInterface.h"
-#include "component_test_mixin.h"
 #include <catch2/catch_test_macros.hpp>
-
-TEST_CASE("CUserInterface follows Component contract",
-          "[unit][Components][CUserInterface]") {
-  // CUserInterface has unique_ptr members, so skip copy tests
-  steamrot::tests::ComponentTestMixin<steamrot::CUserInterface>::TestDefaultConstruction();
-  steamrot::tests::ComponentTestMixin<steamrot::CUserInterface>::TestComponentRegisterIndex();
-  steamrot::tests::ComponentTestMixin<steamrot::CUserInterface>::TestActivation();
-}
 
 TEST_CASE("CUserInterface has correct default values",
           "[unit][Components][CUserInterface]") {
   steamrot::CUserInterface ui;
-  
+
   // Test pre configuration state
   REQUIRE(ui.m_active == false);
   REQUIRE(ui.m_name == "Default UI");
