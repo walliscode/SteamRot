44e7f2dc7d5eb3bf74201bc8cc228b9c33f701de
walliscode <benjaminwallisuk@gmail.com>
2025-12-14 09:41:59 +0000

    removed ISceneConfigurator.cpp

--- src/interfaces/ISceneConfigurator.h
+++ src/interfaces/ISceneConfigurator.h
@@ -40,8 +40,30 @@ public:
   /// @param scene Scene to configure
   /////////////////////////////////////////////////
   std::expected<std::monostate, FailInfo>
-  ConfigureScene(Scene &scene, const SceneData *scene_data);
+  ConfigureScene(Scene &scene, const SceneData *scene_data) {
 
+    // Configure SceneInfo
+    auto info_result = ConfigureSceneInfo(scene, scene_data);
+    if (!info_result.has_value())
+      return std::unexpected(info_result.error());
+
+    // Configure SceneResources
+    auto resources_result = ConfigureSceneResources(scene, scene_data);
+    if (!resources_result.has_value())
+      return std::unexpected(resources_result.error());
+
+    // Configure SceneConfig
+    auto config_result = ConfigureSceneConfig(scene, scene_data);
+    if (!config_result.has_value())
+      return std::unexpected(config_result.error());
+
+    // Configure LogicMap
+    auto logic_result = ConfigureLogicMap(scene);
+    if (!logic_result.has_value())
+      return std::unexpected(logic_result.error());
+
+    return std::monostate();
+  }
   /////////////////////////////////////////////////
   /// @brief Virtual function to configure SceneInfo struct
   ///
@@ -72,6 +94,26 @@ public:
   /// This should be set in stone, not configurable by data or the user.
   /// @param scene  Scene to configure
   /////////////////////////////////////////////////
-  std::expected<std::monostate, FailInfo> ConfigureLogicMap(Scene &scene);
+  std::expected<std::monostate, FailInfo> ConfigureLogicMap(Scene &scene) {
+
+    // Get SceneContext from the scene
+    SceneContext scene_context = scene.GetSceneContext();
+
+    // Get scene type from scene info
+    SceneType scene_type = scene.GetSceneInfo().type;
+
+    // Create LogicFactory
+    LogicFactory logic_factory(scene_type, scene_context);
+
+    // Create logic map
+    auto logic_map_result = logic_factory.CreateLogicMap();
+    if (!logic_map_result.has_value())
+      return std::unexpected(logic_map_result.error());
+
+    // Store logic map in scene resources
+    scene.m_scene_resources.logic_map = std::move(logic_map_result.value());
+
+    return std::monostate();
+  }
 };
 } // namespace steamrot
