f7897725010fbc005020fe534a65d8dfcb421133
walliscode <benjaminwallisuk@gmail.com>
2025-12-20 14:12:34 +0000

    started adding tests

--- tests/unit/user_interface/FlatbuffersUIElementConfigurator.test.cpp
+++ tests/unit/user_interface/FlatbuffersUIElementConfigurator.test.cpp
@@ -41,7 +41,7 @@ LoadUIElementTestData() {
   return {std::move(data), ui_element_data};
 }
 
-TEST_CASE("FlatbuffersUIElementConfigurator initialization",
+TEST_CASE("FlatbuffersUIElementConfigurator error handling",
           "[FlatbuffersUIElementConfigurator]") {
   // set up the test fixture
   steamrot::tests::TestFixture fixture;
@@ -53,4 +53,77 @@ TEST_CASE("FlatbuffersUIElementConfigurator initialization",
   // Initialize configurator
   steamrot::FlatbuffersUIElementConfigurator configurator(
       fixture.GetGameContext().event_handler, *ui_element_data);
+}
+
+TEST_CASE("FlatbuffersUIElementConfigurator configures elements correctly",
+          "[FlatbuffersUIElementConfigurator]") {
+  // set up the test fixture
+  steamrot::tests::TestFixture fixture;
+  // Load UI element test data
+  auto [data, ui_element_data] = LoadUIElementTestData();
+  REQUIRE(ui_element_data != nullptr);
+
+  // Initialize configurator
+  steamrot::FlatbuffersUIElementConfigurator configurator(
+      fixture.GetGameContext().event_handler, *ui_element_data);
+
+  // check fbs data and extract
+  REQUIRE(ui_element_data->root_ui_element()->base_data()->children()->size() ==
+          2);
+
+  auto children = ui_element_data->root_ui_element()->base_data()->children();
+
+  SECTION("Configure PanelElement") {
+    // Create PanelElement from FlatBuffers data
+    auto panel_data = ui_element_data->root_ui_element();
+    REQUIRE(panel_data != nullptr);
+    steamrot::PanelElement panel_element;
+    auto result =
+        configurator.ConfigurePanelElement(panel_element, *panel_data);
+    REQUIRE(result.has_value());
+
+    // add PanelElement specific checks here when needed
+  }
+
+  SECTION("Configure ButtonElement") {
+    // Get ButtonData from FlatBuffers data and check validity
+    auto button_fb = children->Get(0);
+    REQUIRE(button_fb != nullptr);
+    REQUIRE(button_fb->element_type() ==
+            steamrot::UIElementDataUnion::UIElementDataUnion_ButtonData);
+    auto button_data =
+        static_cast<const steamrot::ButtonData *>(button_fb->element());
+    REQUIRE(button_data != nullptr);
+
+    // create ButtonElement and configure using flatbuffers data
+    steamrot::ButtonElement button_element;
+    auto result =
+        configurator.ConfigureButtonElement(button_element, *button_data);
+    REQUIRE(result.has_value());
+
+    // ButtnonElement specific checks
+    REQUIRE(button_element.label == "Test Tab");
+  }
+
+  SECTION("Configure DropDownListElement") {
+    // Get DropDownListData from FlatBuffers data and check validity
+    auto dropdown_fb = children->Get(1);
+    REQUIRE(dropdown_fb != nullptr);
+    REQUIRE(dropdown_fb->element_type() ==
+            steamrot::UIElementDataUnion::UIElementDataUnion_DropDownListData);
+    auto dropdown_data =
+        static_cast<const steamrot::DropDownListData *>(dropdown_fb->element());
+    REQUIRE(dropdown_data != nullptr);
+    // create DropDownListElement and configure using flatbuffers data
+    steamrot::DropDownListElement dropdown_element;
+    auto result = configurator.ConfigureDropDownListElement(dropdown_element,
+                                                            *dropdown_data);
+    REQUIRE(result.has_value());
+    // DropDownListElement specific checks
+    REQUIRE(dropdown_element.unexpanded_label == "Select Option");
+    REQUIRE(dropdown_element.expanded_label == "Options:");
+    REQUIRE(dropdown_element.data_populate_function ==
+            steamrot::DataPopulateFunction::
+                DataPopulateFunction_PopulateWithFragmentData);
+  }
 }
