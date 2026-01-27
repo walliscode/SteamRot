6de39addc500dea500bcae3b614a25bf8105304c
walliscode <benjaminwallisuk@gmail.com>
2026-01-27 08:13:55 +0000

    matchers library and tests up and running

--- tests/matchers/JointEqualsMatcher.cpp
+++ tests/matchers/JointEqualsMatcher.cpp
@@ -20,26 +20,26 @@ bool JointEqualsMatcher::match(const Joint &actual) const {
   m_mismatch_description.clear();
   std::ostringstream oss;
 
-  if (actual.m_joint_name != m_expected.m_joint_name) {
-    oss << "m_joint_name: actual='" << actual.m_joint_name << "', expected='"
-        << m_expected.m_joint_name << "'; ";
+  if (actual.name != m_expected.name) {
+    oss << "m_joint_name: actual='" << actual.name << "', expected='"
+        << m_expected.name << "'; ";
   }
 
-  if (actual.m_number_of_connections != m_expected.m_number_of_connections) {
+  if (actual.connection_number != m_expected.connection_number) {
     oss << "m_number_of_connections differs; ";
   }
 
-  if (actual.m_global_position != m_expected.m_global_position) {
+  if (actual.global_position != m_expected.global_position) {
     oss << "m_global_position differs; ";
   }
 
-  if (actual.m_connected_fragments != m_expected.m_connected_fragments) {
+  if (actual.connected_fragments != m_expected.connected_fragments) {
     oss << "m_connected_fragments differs; ";
   }
 
   // Compare transforms (matrix elements)
-  const auto *this_matrix = actual.m_transform.getMatrix();
-  const auto *other_matrix = m_expected.m_transform.getMatrix();
+  const auto *this_matrix = actual.transform.getMatrix();
+  const auto *other_matrix = m_expected.transform.getMatrix();
   bool transform_matches = true;
   for (size_t i = 0; i < 16; ++i) {
     if (this_matrix[i] != other_matrix[i]) {
@@ -51,8 +51,8 @@ bool JointEqualsMatcher::match(const Joint &actual) const {
     oss << "m_transform differs; ";
   }
 
-  if (actual.m_render_overlay.getVertexCount() !=
-      m_expected.m_render_overlay.getVertexCount()) {
+  if (actual.render_overlay.getVertexCount() !=
+      m_expected.render_overlay.getVertexCount()) {
     oss << "m_render_overlay vertex count differs; ";
   }
 
@@ -64,7 +64,7 @@ bool JointEqualsMatcher::match(const Joint &actual) const {
 std::string JointEqualsMatcher::describe() const {
   if (m_mismatch_description.empty()) {
     std::ostringstream oss;
-    oss << "equals Joint(m_joint_name='" << m_expected.m_joint_name << "')";
+    oss << "equals Joint(m_joint_name='" << m_expected.name << "')";
     return oss.str();
   }
   return "Joint mismatch: " + m_mismatch_description;
