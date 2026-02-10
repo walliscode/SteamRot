0aad80af04a6fde2a4aacfb1340ccece3515266b
walliscode <benjaminwallisuk@gmail.com>
2026-02-10 22:08:45 +0000

    MachinaFormScaffold added to GrimoireMachina

--- src/logic/action_grimoire_machina.cpp
+++ src/logic/action_grimoire_machina.cpp
@@ -8,28 +8,28 @@
 /// Headers
 /////////////////////////////////////////////////
 #include "action_grimoire_machina.h"
-#include "MachinaForm.h"
+#include "MachinaFormScaffold.h"
 
 namespace steamrot::logic::actions::grimoire_machina {
 /////////////////////////////////////////////////
 std::expected<std::monostate, FailInfo>
-InitialiseActiveMachinaForm(GrimoireMachina &grimoire_machina) {
+InitialiseActiveMachinaFormScaffold(GrimoireMachina &grimoire_machina) {
 
   // clear the active form if it exists
-  if (grimoire_machina.m_active_form)
-    grimoire_machina.m_active_form.reset();
+  if (grimoire_machina.m_scaffold_form)
+    grimoire_machina.m_scaffold_form.reset();
   // add a new MachinaForm to the active form
-  grimoire_machina.m_active_form = std::make_unique<MachinaForm>();
+  grimoire_machina.m_scaffold_form = std::make_unique<MachinaFormScaffold>();
 
   return std::monostate{};
 }
 
 /////////////////////////////////////////////////
 std::expected<std::monostate, FailInfo>
-ClearActiveMachinaForm(GrimoireMachina &grimoire_machina) {
+ClearActiveMachinaFormScaffold(GrimoireMachina &grimoire_machina) {
   // clear the active form if it exists
-  if (grimoire_machina.m_active_form)
-    grimoire_machina.m_active_form.reset();
+  if (grimoire_machina.m_scaffold_form)
+    grimoire_machina.m_scaffold_form.reset();
 
   return std::monostate{};
 }
--- tests/unit/logic/action_grimoire_machina.test.cpp
+++ tests/unit/logic/action_grimoire_machina.test.cpp
@@ -7,6 +7,7 @@
 /// Headers
 /////////////////////////////////////////////////
 #include "action_grimoire_machina.h"
+#include "MachinaFormScaffold.h"
 #include <catch2/catch_test_macros.hpp>
 
 TEST_CASE("InitialiseActiveMachinaForm adds a new MachinaForm to the "
@@ -15,21 +16,20 @@ TEST_CASE("InitialiseActiveMachinaForm adds a new MachinaForm to the "
           "[GrimoireMachina]") {
   steamrot::GrimoireMachina grimoire_machina;
 
-  auto result =
-      steamrot::logic::actions::grimoire_machina::InitialiseActiveMachinaForm(
-          grimoire_machina);
+  auto result = steamrot::logic::actions::grimoire_machina::
+      InitialiseActiveMachinaFormScaffold(grimoire_machina);
   REQUIRE(result.has_value());
-  REQUIRE(grimoire_machina.m_active_form != nullptr);
+  REQUIRE(grimoire_machina.m_scaffold_form != nullptr);
 }
 
 TEST_CASE("ClearActiveMachinaForm clears the active MachinaForm in the "
           "GrimoireMachina",
           "[GrimoireMachina]") {
   steamrot::GrimoireMachina grimoire_machina;
-  grimoire_machina.m_active_form = std::make_unique<steamrot::MachinaForm>();
-  auto result =
-      steamrot::logic::actions::grimoire_machina::ClearActiveMachinaForm(
-          grimoire_machina);
+  grimoire_machina.m_scaffold_form =
+      std::make_unique<steamrot::MachinaFormScaffold>();
+  auto result = steamrot::logic::actions::grimoire_machina::
+      ClearActiveMachinaFormScaffold(grimoire_machina);
   REQUIRE(result.has_value());
-  REQUIRE(grimoire_machina.m_active_form == nullptr);
+  REQUIRE(grimoire_machina.m_scaffold_form == nullptr);
 }
