580a88155f321f4c8a3e9c5a2fdb3264d39248f8
walliscode <benjaminwallisuk@gmail.com>
2026-05-23 11:14:20 +0100

    fleshed out builder class

--- tests/part_graphs/PartGraphBuilder.cpp
+++ tests/part_graphs/PartGraphBuilder.cpp
@@ -1,24 +1,124 @@
 /////////////////////////////////////////////////
 /// @file
-/// @brief Declaration of the PartGraphBuilder class, which is used to build
-/// part graphs for testing purposes.
+/// @brief Implementation of the PartGraphBuilder class
 /////////////////////////////////////////////////
-
-/////////////////////////////////////////////////
-/// Preprocessor Directives
-/////////////////////////////////////////////////
-#pragma once
 
 /////////////////////////////////////////////////
 /// Headers
 /////////////////////////////////////////////////
+#include "PartGraphBuilder.h"
+#include "action_grimoire_machina.h"
+#include "catch2/catch_test_macros.hpp"
+#include "part_library.h"
 
-#include "MachinaFormScaffold.h"
 namespace steamrot::tests {
+/////////////////////////////////////////////////
+FragmentInstance
+PartGraphBuilder::MakeFragmentInstance(const FragmentNames name) {
+  const Fragment *f;
+  switch (name) {
+  case FragmentNames::NoSocket:
+    f = &fragment_no_socket;
+    break;
+  case FragmentNames::OneSocket:
+    f = &fragment_one_socket;
+    break;
+  case FragmentNames::TwoSockets:
+    f = &fragment_two_sockets;
+    break;
+  case FragmentNames::ThreeSockets:
+    f = &fragment_three_sockets;
+    break;
+  }
+  return FragmentInstance{f};
+}
 
-struct PartGraphPackage {
-  PartGraph graph;
-};
+/////////////////////////////////////////////////
+JointInstance PartGraphBuilder::MakeJointInstance(const JointNames name) {
+  const Joint *j;
+  switch (name) {
+  case JointNames::NoSocket:
+    j = &joint_no_socket;
+    break;
+  case JointNames::OneSocket:
+    j = &joint_one_socket;
+    break;
+  case JointNames::TwoSockets:
+    j = &joint_two_sockets;
+    break;
+  case JointNames::ThreeSockets:
+    j = &joint_three_sockets;
+    break;
+  }
+  return JointInstance{j};
+}
 
-class PartGraphBuilder {};
+/////////////////////////////////////////////////
+PartGraphBuilder &PartGraphBuilder::AddFragment(const FragmentNames name,
+                                                const std::string id) {
+
+  // create a new FragmentInstance and add it to the part graph with the next
+  // available ID
+  FragmentInstance instance = MakeFragmentInstance(name);
+  const uint32_t instance_id = m_package.next_id;
+  m_package.part_graph.emplace(instance_id, instance);
+  m_package.next_id++;
+
+  // map the user-friendly string ID to the stable uint32_t ID in the part graph
+  m_package.id_to_part_graph_id.emplace(id, instance_id);
+
+  return *this;
+}
+
+/////////////////////////////////////////////////
+PartGraphBuilder &PartGraphBuilder::AddJoint(const JointNames name,
+                                             const std::string id) {
+  // create a new JointInstance and add it to the part graph with the next
+  // available ID
+  JointInstance instance = MakeJointInstance(name);
+  const uint32_t instance_id = m_package.next_id;
+  m_package.part_graph.emplace(instance_id, instance);
+  m_package.next_id++;
+
+  // map the user-friendly string ID to the stable uint32_t ID in the part graph
+  m_package.id_to_part_graph_id.emplace(id, instance_id);
+  return *this;
+}
+/////////////////////////////////////////////////
+PartGraphBuilder &PartGraphBuilder::Connect(const std::string &from_id,
+                                            const uint32_t from_socket_id,
+                                            const std::string &to_id,
+                                            const uint32_t to_socket_id) {
+  // look up the stable uint32_t IDs for the from and to parts using the string
+  // IDs provided by the user
+  // if either ID is not found, throw an exception
+  const auto from_it = m_package.id_to_part_graph_id.find(from_id);
+  if (from_it == m_package.id_to_part_graph_id.end()) {
+    FAIL("from_id '" << from_id << "' not found in id_to_part_graph_id");
+  }
+  const auto to_it = m_package.id_to_part_graph_id.find(to_id);
+  if (to_it == m_package.id_to_part_graph_id.end()) {
+    FAIL("to_id '" << to_id << "' not found in id_to_part_graph_id");
+  }
+  const uint32_t from_part_id = from_it->second;
+  const uint32_t to_part_id = to_it->second;
+
+  // use the create connection function that already exists in the logic code to
+  // create the connection between the two parts
+  auto result = steamrot::logic::action::grimoire_machina::create_connection(
+      std::get<FragmentInstance>(m_package.part_graph.at(from_part_id)),
+      from_socket_id,
+      std::get<JointInstance>(m_package.part_graph.at(to_part_id)),
+      to_socket_id);
+  // return *this to allow chaining of Connect calls
+  return *this;
+}
+/////////////////////////////////////////////////
+PartGraphPackage PartGraphBuilder::Build() {
+  // create copy of the package to return, then reset the builder's internal
+  // state for potential reuse
+  PartGraphPackage package_copy = m_package;
+  m_package = PartGraphPackage{};
+  return package_copy;
+}
 } // namespace steamrot::tests
---
+++ tests/part_graphs/PartGraphBuilder.h
@@ -0,0 +1,90 @@
+/////////////////////////////////////////////////
+/// @file
+/// @brief Declaration of the PartGraphBuilder class.
+/////////////////////////////////////////////////
+
+/////////////////////////////////////////////////
+/// Preprocessor Directives
+/////////////////////////////////////////////////
+#pragma once
+
+/////////////////////////////////////////////////
+/// Headers
+/////////////////////////////////////////////////
+
+#include "MachinaFormScaffold.h"
+#include <unordered_map>
+namespace steamrot::tests {
+
+enum class FragmentNames { NoSocket, OneSocket, TwoSockets, ThreeSockets };
+
+enum class JointNames { NoSocket, OneSocket, TwoSockets, ThreeSockets };
+
+/////////////////////////////////////////////////
+/// @class PartGraphPackage
+/// @brief All data necessary to test predicates against a PargGraph and
+/// generate a trace
+/////////////////////////////////////////////////
+struct PartGraphPackage {
+  /////////////////////////////////////////////////
+  /// @brief stable ID generator for new parts added to the graph. Incremented
+  /// by the builder for each new part added, ensuring unique IDs across both
+  /// joints
+  /////////////////////////////////////////////////
+  uint32_t next_id{0};
+
+  /////////////////////////////////////////////////
+  /// @brief PartGraph containing Part Instances and their connections.
+  /////////////////////////////////////////////////
+  PartGraph part_graph;
+
+  /////////////////////////////////////////////////
+  /// @brief Mapping from user-friendly string IDs to stable uint32_t IDs in the
+  /// PartGraph.
+  /// used by the trace generator to translate the user-friendly IDs used in the
+  /// test definitions
+  /////////////////////////////////////////////////
+  std::unordered_map<std::string, uint32_t> id_to_part_graph_id{};
+};
+
+class PartGraphBuilder {
+
+private:
+  PartGraphPackage m_package{};
+
+public:
+  /////////////////////////////////////////////////
+  /// @brief Default constructor. Initializes an empty PartGraphBuilder.
+  /////////////////////////////////////////////////
+  PartGraphBuilder() = default;
+
+  /////////////////////////////////////////////////
+  /// @brief Creates a FragmentInstance based on the provided FragmentNames enum
+  /// value.
+  ///
+  /// @param name Enum value specifying the type of FragmentInstance to create.
+  /// @return A FragmentInstance corresponding to the specified FragmentNames.
+  /////////////////////////////////////////////////
+  FragmentInstance MakeFragmentInstance(const FragmentNames name);
+
+  /////////////////////////////////////////////////
+  /// @brief Creates a JointInstance based on the provided JointNames enum
+  /// value.
+  ///
+  /// @param name Enum value specifying the type of JointInstance to create.
+  /// @return A JointInstance corresponding to the specified JointNames.
+  /////////////////////////////////////////////////
+  JointInstance MakeJointInstance(const JointNames name);
+
+  PartGraphBuilder &AddFragment(const FragmentNames name, const std::string id);
+
+  PartGraphBuilder &AddJoint(const JointNames name, const std::string id);
+
+  PartGraphBuilder &Connect(const std::string &from_id,
+                            const uint32_t from_socket_id,
+                            const std::string &to_id,
+                            const uint32_t to_socket_id);
+  PartGraphPackage Build();
+};
+
+} // namespace steamrot::tests
